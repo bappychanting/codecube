90fbe81c5afbaec9776554ec00737e7d0d71ae64
bappychanting <bappychanting@outlook.com>
2022-10-19 18:42:31 +0600

    updated index

--- index.php
+++ index.php
@@ -33,30 +33,20 @@ try{
             echo json_encode($messages);
         }
     }
-    else
-    {
+    else{
             // Create route url string
         $route_url = ltrim(strtok($_SERVER['REQUEST_URI'], '?'), '/');
 
             // Checking if api url
-        if(strpos($route_url, $default['api_url'].'/') === 0){
+        if(strpos($route_url, $default['api_url'].'/') === 0)
+        {
+                // Include api routes
+            $routes = include($config_files['api_routes']); 
 
                 // Rewrite URL
-            $api_url = substr($route_url, strlen('api/'));
-
-                // Include api routes
-            $routes = include($config_files['api_routes']);
-
-                // Call api route
-            if(empty($routes[$api_url]))
-                call($default['error']);
-            else
-                call($routes[$api_url]);
+            $route_url = empty(substr($route_url, strlen($default['api_url'].'/'))) ? '404' : substr($route_url, strlen($default['api_url'].'/'));
         }
         else{
-                // Include Web Routes
-            $routes = include($config_files['web_routes']);
-
                 // Include project application configuration files and setting up
             $config = include($config_files['app']);
             serverSetup($config);
@@ -64,17 +54,20 @@ try{
                 // Starting session
             session_start();
 
+                // Include Web Routes
+            $routes = include($config_files['web_routes']);
+
                 // Sanitizing url and incoming parameters
             $route_url = sanitize($route_url, $routes);
-
-                // Call route
-            if(empty($route_url))
-                call($default['landing']);
-            elseif(empty($routes[$route_url]))
-                call($default['error']);
-            else
-                call($routes[$route_url]);
         }
+
+        // Call route
+        if(empty($route_url))
+            call($default['landing']);
+        elseif(empty($routes[$route_url]))
+            call($default['error']);
+        else
+            call($routes[$route_url]);
 
             // Log last occured error
         if(!empty(error_get_last()))    logger('ERROR: '.error_get_last()['message'].' in '.error_get_last()['file'].' in '.error_get_last()['line']);
